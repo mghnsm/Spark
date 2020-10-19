6214ffae2815be0b6e3854dd11ce08435e99ddc7
Anna <mghnsm@gmail.com>
2020-10-19 23:46:47 +0300

    Scripted auto-commit on change (2020-10-19 23:46:47) by gitwatch.sh

--- src/main/java/bmstu/SparkApp.java
+++ src/main/java/bmstu/SparkApp.java
@@ -4,11 +4,16 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
+import java.util.Arrays;
+
 public class SparkApp {
     public static void main(String[] args) throws Exception {
         SparkConf conf = new SparkConf().setAppName("lab5");
         JavaSparkContext sc = new JavaSparkContext(conf);
         JavaRDD<String> dict = sc.textFile("L_AIRPORT_ID.csv");
         JavaRDD<String> time = sc.textFile("664600583_T_ONTIME_sample.csv");
+        JavaRDD<String> dictSplitted = dict.flatMap(s -> Arrays.stream(s.split(",")).iterator());
+        JavaRDD<String> timeSplitted = time.flatMap(s -> Arrays.stream(s.split(",")).iterator());
+        
     }
 }
