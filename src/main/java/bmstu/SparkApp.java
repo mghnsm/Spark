eb6c9e6061e0aeb1e21859263a81844604cb71cf
Anna <mghnsm@gmail.com>
2020-10-19 23:26:58 +0300

    Scripted auto-commit on change (2020-10-19 23:26:58) by gitwatch.sh

--- src/main/java/bmstu/SparkApp.java
+++ src/main/java/bmstu/SparkApp.java
@@ -8,6 +8,6 @@ public class SparkApp {
     public static void main(String[] args) throws Exception {
         SparkConf conf = new SparkConf().setAppName("lab5");
         JavaSparkContext sc = new JavaSparkContext(conf);
-        JavaRDD<String> dict = 
+        JavaRDD<String> dict = sc.textFile();
     }
 }
