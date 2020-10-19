e13fb1e2b1ddb580b0d5843cefbac2eec887c5dc
Anna <mghnsm@gmail.com>
2020-10-19 23:24:03 +0300

    Scripted auto-commit on change (2020-10-19 23:24:03) by gitwatch.sh

--- src/main/java/bmstu/SparkApp.java
+++ src/main/java/bmstu/SparkApp.java
@@ -1,7 +1,9 @@
 package bmstu;
 
+import org.apache.spark.SparkConf;
+
 public class SparkApp {
     public static void main(String[] args) throws Exception {
-        
+        SparkConf 
     }
 }
