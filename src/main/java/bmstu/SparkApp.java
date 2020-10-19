0a5ba4bbd905e41b307294b642c14e83ae35459b
Anna <mghnsm@gmail.com>
2020-10-19 23:26:23 +0300

    Scripted auto-commit on change (2020-10-19 23:26:23) by gitwatch.sh

--- src/main/java/bmstu/SparkApp.java
+++ src/main/java/bmstu/SparkApp.java
@@ -1,12 +1,13 @@
 package bmstu;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
 public class SparkApp {
     public static void main(String[] args) throws Exception {
         SparkConf conf = new SparkConf().setAppName("lab5");
         JavaSparkContext sc = new JavaSparkContext(conf);
-        JavaRDD<String>
+        JavaRDD<String> dict = 
     }
 }
