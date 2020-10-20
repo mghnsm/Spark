7b6e2de84c9280bbf6be6471f52beb1d20a6ca0c
Anna <mghnsm@gmail.com>
2020-10-20 16:06:02 +0300

    Scripted auto-commit on change (2020-10-20 16:06:02) by gitwatch.sh

--- src/main/java/bmstu/AirportInfo.java
+++ src/main/java/bmstu/AirportInfo.java
@@ -2,10 +2,17 @@ package bmstu;
 
 public class AirportInfo {
     private String[] str;
+    private int 
 
     public AirportInfo(String[] str) {
         this.str = str;
     }
 
-    
+    public String getAirportID() {
+        return str[0];
+    }
+
+    public String getAirportName() {
+        return str[1];
+    }
 }
