96f6ec3ee3a920fdfef353ddd05094a6bbe6ad36
Anna <mghnsm@gmail.com>
2020-10-20 16:24:23 +0300

    Scripted auto-commit on change (2020-10-20 16:24:22) by gitwatch.sh

--- src/main/java/bmstu/FlightInfo.java
+++ src/main/java/bmstu/FlightInfo.java
@@ -4,8 +4,8 @@ public class FlightInfo {
     private String[] str;
     private static final int ARR_DELAY_NEW;
     private static final int CANCELLED;
-    private static final int ORIGIN_AIRPORT_ID;
-    private static final int DEST_AIRPORT_ID;
+    private static final int ORIGIN_AIRPORT_ID = 11;
+    private static final int DEST_AIRPORT_ID = 14;
 
     public FlightInfo(String[] str) {
         this.str = str;
