4efc694371a6faefcd3d5d7ac2d292446c104685
Anna <mghnsm@gmail.com>
2020-10-20 16:27:00 +0300

    Scripted auto-commit on change (2020-10-20 16:27:00) by gitwatch.sh

--- src/main/java/bmstu/FlightInfo.java
+++ src/main/java/bmstu/FlightInfo.java
@@ -2,20 +2,28 @@ package bmstu;
 
 public class FlightInfo {
     private String[] str;
-    private static final int ARR_DELAY_NEW = 18;
-    private static final int CANCELLED;
     private static final int ORIGIN_AIRPORT_ID = 11;
     private static final int DEST_AIRPORT_ID = 14;
+    private static final int ARR_DELAY_NEW = 18;
+    private static final int CANCELLED = 19;
 
     public FlightInfo(String[] str) {
         this.str = str;
     }
 
-    public String isCancelled() {
+    public String getOriginAirportID() {
+        return str[ORIGIN_AIRPORT_ID];
+    }
 
+    public String getDestAirportID() {
+        return str[DEST_AIRPORT_ID];
+    }
+
+    public String isCancelled() {
+        return str[CANCELLED];
     }
 
     public String getDelayTime() {
-
+        return str[ARR_DELAY_NEW];
     }
 }
