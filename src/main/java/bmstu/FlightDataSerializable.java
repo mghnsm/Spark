37454589aadead9af660db2dd09264ba623ff5bf
Anna <mghnsm@gmail.com>
2020-10-22 23:25:05 +0300

    Scripted auto-commit on change (2020-10-22 23:25:05) by gitwatch.sh

--- src/main/java/bmstu/FlightDataSerializable.java
+++ src/main/java/bmstu/FlightDataSerializable.java
@@ -3,6 +3,9 @@ package bmstu;
 import java.io.Serializable;
 
 public class FlightDataSerializable implements Serializable {
+    private int allFlights;
+    
+
     public FlightDataSerializable() {
 
     }
