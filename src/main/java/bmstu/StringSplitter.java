bb5225972fb3ec9da890c5faa90b7ef7896bf690
Anna <mghnsm@gmail.com>
2020-10-23 00:13:16 +0300

    Scripted auto-commit on change (2020-10-23 00:13:16) by gitwatch.sh

--- src/main/java/bmstu/StringSplitter.java
+++ src/main/java/bmstu/StringSplitter.java
@@ -1,5 +1,13 @@
 package bmstu;
 
 public class StringSplitter {
-    
+    private String[] splitted;
+
+    public StringSplitter(String str) {
+        splitted = str.split(",");
+    }
+
+    public String[] getSplitted() {
+        return splitted;
+    }
 }
