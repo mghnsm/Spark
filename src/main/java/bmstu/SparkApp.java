fa9c6d0cc2caf200d9fa2fc426f95cacd4a09d28
Anna <mghnsm@gmail.com>
2020-11-06 19:52:45 +0300

    Scripted auto-commit on change (2020-11-06 19:52:45) by gitwatch.sh

--- src/main/java/bmstu/SparkApp.java
+++ src/main/java/bmstu/SparkApp.java
@@ -19,6 +19,7 @@ public class SparkApp {
         JavaRDD<String> dictSplitted = dict.flatMap(s -> Arrays.stream(s.split(",")).iterator());
         JavaRDD<String> timeSplitted = time.flatMap(s -> Arrays.stream(s.split(",")).iterator());
 
+        
 
         //final Broadcast<Map<String, AirportData>> airportsBroadcasted = sc.broadcast(stringAirportDataMap);
     }
