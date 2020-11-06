e15e7d7d323cf36af2b444ad90727257e26b1fd4
Anna <mghnsm@gmail.com>
2020-11-06 22:03:59 +0300

    Scripted auto-commit on change (2020-11-06 22:03:59) by gitwatch.sh

--- src/main/java/bmstu/SparkApp.java
+++ src/main/java/bmstu/SparkApp.java
@@ -47,7 +47,7 @@ public class SparkApp {
         JavaPairRDD<Tuple2, FlightDataSerializable> flightPairsTotal = flightPairs
                 .reduceByKey(FlightDataSerializable::addData);
 
-        JavaPairRDD<Tuple2, > xx = airportSplitted;
+        Map<> =
 
         //flightPairsTotal.saveAsTextFile("output");
 
