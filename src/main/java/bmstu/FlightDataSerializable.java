0938ca8469a36cf5aba8250489651911145bc2d6
Anna <mghnsm@gmail.com>
2020-11-06 20:51:18 +0300

    Scripted auto-commit on change (2020-11-06 20:51:18) by gitwatch.sh

--- src/main/java/bmstu/FlightDataSerializable.java
+++ src/main/java/bmstu/FlightDataSerializable.java
@@ -16,7 +16,15 @@ public class FlightDataSerializable implements Serializable {
     }
 
     public FlightDataSerializable(String delay, String cancelled) {
-        
+        if (delay.equals("")) {
+            this.maxDelayTime = 0;
+            this.delayedFlights = 0;
+        } else {
+            this.maxDelayTime = Float.parseFloat(delay);
+            this.delayedFlights = (maxDelayTime > 0 ? 1 : 0);
+        }
+        this.allFlights = 1;
+        this.cancelledFlights = (Float.parseFloat(cancelled) > 0 ? 1 : 0);
     }
 
     public int getAllFlights() {
