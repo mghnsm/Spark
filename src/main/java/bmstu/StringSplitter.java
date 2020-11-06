86dbde045035fb4f141df8b715fdf11fc1e9b301
Anna <mghnsm@gmail.com>
2020-11-06 19:57:31 +0300

    Scripted auto-commit on change (2020-11-06 19:57:31) by gitwatch.sh

--- src/main/java/bmstu/StringSplitter.java
+++ src/main/java/bmstu/StringSplitter.java
@@ -1,8 +1,11 @@
 package bmstu;
 
 public class StringSplitter {
-    public static String[] split(String str) {
-        String[] splitted = str.split(",");
+    public static String[] split(String input) {
+        String[] splitted = input.split(",");
+        for (int i = 0; i < splitted.length; i++) {
+            splitted[i] = splitted[i].replaceAll("\"", "");
+        }
         return splitted;
     }
 }
